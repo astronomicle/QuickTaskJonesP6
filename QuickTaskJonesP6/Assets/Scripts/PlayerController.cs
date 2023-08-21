6782ae81199d0f460b1976e97d1475e95efa6e6e
astronomicle <719168@student.dusd.net>
2023-08-21 13:50:38 -0700

    Started quick task
    I made almost all the assets and started coding

---
+++ QuickTaskJonesP6/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float verticalInput;
+    public float horizontalInput;
+    public float speed = 10f;
+
+    void Start()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+
+    void Update()
+    {
+        
+    }
+}
